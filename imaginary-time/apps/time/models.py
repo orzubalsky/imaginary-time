926483bb04d0f204c9d24f2f2744eb20d4ed4bb8
Or Zubalsky <juviley@gmail.com>
2012-12-09 14:30:44 -0500

    removed unused code

--- imaginary-time/apps/time/models.py
+++ imaginary-time/apps/time/models.py
@@ -1,25 +1,12 @@
 from django.conf import settings
-from django.contrib.gis.db.models import *
-from django.utils.timezone import utc
-from django.contrib.auth.models import User
-from django.core.files.base import ContentFile        	
-from django.conf import settings
-from django.core.files import File
-from django.core.files.storage import default_storage as storage        
-from django.db.models.signals import pre_delete, post_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver  
-from django.core.cache import cache
-from django_countries import CountryField
-from taggit.managers import TaggableManager      
 from datetime import *
-from futures.validators import *
 import os, sys, pytz, uuid, random
 
 
 class Base(Model):
-    """
-    Base model for all of the models in ts.  
-    """
+    """Base model for all of the models in the app. """
     class Meta:
             abstract = True
                     
@@ -32,160 +19,29 @@ class Base(Model):
             return self.title
         else:
             return "%s" % (type(self))
-            
-class UserProfile(Base):
-    user                = OneToOneField(User)
-    display_name        = CharField(max_length=50)
-    city                = CharField(max_length=100, blank=True, null=True)
-    state               = CharField(max_length=255, blank=True, null=True)
-    country             = CountryField(blank=True, null=True)
-    number              = CharField(max_length=30, blank=True, null=True)
-    country_code        = CharField(max_length=10, blank=True, null=True)
-    slug                = SlugField()    
 
 
-class Collection(Base):
-    title               = CharField(max_length=100, blank=True, null=True)
-    description         = TextField(blank=True, null=True)
+class Response(Base):
+    """Stores values of a single response to the question posted on the site """
 
-
-class GeoSound(Base):
-    class Meta:
-        verbose_name_plural = "geosounds"
-                     
-    def random_z():
-        return round(random.uniform(-12.0, 12.0), 2)
-        
-    def random_default_volume():
-        return round(random.uniform(0.2, 0.8), 2)
-                          
-    sound               = FileField(upload_to="uploads", max_length=150)
-    title               = CharField(max_length=100, blank=True, null=True)
-    location            = CharField(max_length=150, blank=True, null=True)
-    story               = TextField(blank=True, null=True)
-    created_by          = CharField(max_length=100, blank=False, null=True)    
-    user                = ForeignKey(User, blank=True, null=True)
-    slug                = SlugField(max_length=100)    
-    point               = PointField()
-    z                   = FloatField(default=random_z)
-    default_volume      = FloatField(default=random_default_volume)
-    collections         = ManyToManyField(Collection, related_name="collections")    
-    tags                = TaggableManager()    
-    
-    objects = GeoManager()
-    
-    def is_recent():
-        def fget(self):
-            now = datetime.utcnow().replace(tzinfo=utc)
-            week_ago = now - timedelta(days=7)
-            return self.created > week_ago        
-        return locals()
-        
-    def just_added():
-        def fget(self):
-            now = datetime.utcnow().replace(tzinfo=utc)
-            minute_ago = now - timedelta(seconds=60)
-            return self.created > minute_ago        
-        return locals()        
-        
-    is_recent  = property(**is_recent())
-    just_added = property(**just_added())    
-    
-    def save_upload(self, filename, lat, lon, tags, *args, **kwargs):
-        from django.contrib.gis.geos import Point
-        
-        "save geosound after ajax uploading an mp3 file"
-
-        # store point from coordinates
-        self.point = Point(lon, lat, srid=4326)
-
-        # try finding an existing user by the "created_by" field
-        try:
-            self.user = User.objects.get(username=self.created_by)
-        except User.DoesNotExist:
-            pass
-        
-        # create a title for the sound
-        self.title = "recorded in %s by %s" % (self.location, self.created_by)
-        
-        # save sound
-        self.sound = filename
-                    
-        # save model
-        super(GeoSound, self).save(*args, **kwargs)
-        
-        # save tags to sound
-        for t in tags:
-            self.tags.add(t)
-        
-        # connect the sound to the v3 collection
-        v3_collection, created = Collection.objects.get_or_create(title='fantastic futures v3', defaults={'title': 'fantastic futures v3'})
-        self.collections.add(v3_collection)
-        
-        # return the newly created model
-        return self
+    dream   = FloatField(default= 0.0)
+    make    = FloatField(default= 0.0)
+    learn   = FloatField(default= 0.0)
+    teach   = FloatField(default= 0.0)
+    rest    = FloatField(default= 0.0)
+    taste   = FloatField(default= 0.0)
+    play    = FloatField(default= 0.0)
+    earn    = FloatField(default= 0.0)
+    spend   = FloatField(default= 0.0)
+    connect = FloatField(default= 0.0)
+    move    = FloatField(default= 0.0)
+    help    = FloatField(default= 0.0)
+    sent_by = IPAddressField()
 
     def __unicode__(self):
-        return self.title
-        
-    def get_tags(self):
-        return ",".join([tag.name for tag in self.tags.all()])   
-        
-        
-@receiver(pre_delete, sender=GeoSound)
-@receiver(post_save, sender=GeoSound)
-def invalidate_json_sounds(sender, **kwargs):
-    cache.delete('json_sounds')
-        
-class Connection(Base):
-
-    sound_1         = ForeignKey(GeoSound, related_name="sound_1")
-    sound_1_volume  = FloatField(default = 0.8)
-    sound_2         = ForeignKey(GeoSound, related_name="sound_2")
-    sound_2_volume  = FloatField(default = 0.8)
-    
-    def __unicode__(self):
-        return "%s - %s" % (self.sound_1.title, self.sound_2.title)
+        return "%s response" % sent_by
 
 
-class Constellation(Base):
-    class Meta:
-        verbose_name_plural = "constellations"
-        
-    title               = CharField(max_length=100, blank=False, null=False)
-    created_by          = CharField(max_length=100, blank=False, null=True)
-    location            = CharField(max_length=150, blank=True, null=True)
-    user                = ForeignKey(User, blank=True, null=True)
-    slug                = SlugField()
-    connections         = ManyToManyField(Connection, related_name="connections")
-    rotation_x          = FloatField(default=0)
-    rotation_y          = FloatField(default=0)
-    rotation_z          = FloatField(default=0)
-    zoom                = FloatField(default=1.0)
-    
-    def __unicode__(self):
-        return self.title
-        
-    def save_ajax(self, rotation, *args, **kwargs):
-
-        # try finding an existing user by the "created_by" field
-        try:
-            self.user = User.objects.get(username=self.created_by)
-        except User.DoesNotExist:
-            pass
-            
-        # rotation
-        self.rotation_x = rotation['x']
-        self.rotation_y = rotation['y']
-        self.rotation_z = rotation['z']
-        
-        # save model
-        super(Constellation, self).save(*args, **kwargs)
-
-        # return the newly created model
-        return self       
-
-@receiver(pre_delete, sender=Constellation)
-@receiver(post_save, sender=Constellation)
-def invalidate_json_constellations(sender, **kwargs):
-    cache.delete('json_constellations')
+@receiver(post_save, sender=Response)
+def email_response(sender, **kwargs):
+    # format values in an email and send to the knitters
--- imaginary-time/apps/time/urls.py
+++ imaginary-time/apps/time/urls.py
@@ -1,10 +1,5 @@
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('futures.views',
-    url(r'ajax-upload$', 'import_uploader', name="ajax-upload"),    
-    url(r'start$', 'start', name="start"),
-    url(r'11/09/12$', 'performance', name="performance"),
-    url(r'^sound/(?P<sound_slug>[-\w]+)$', 'view_sound', name='view-sound'),
-    url(r'^sound-layer$', 'sound_layer', name='sound-layer'),    
+urlpatterns = patterns('time.views',
     url(r'^$', 'index', name='home'),    
 )
